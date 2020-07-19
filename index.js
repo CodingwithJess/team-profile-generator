519533e2e0b41e85bc36320b01af8d6bb4731510
CodingwithJess <thejessiebess@gmail.com>
2020-07-19 11:42:52 -0600

    added functions for user prompts and started function to generate basic index.HTML

---
+++ index.js
@@ -0,0 +1,73 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+    },
+    {
+      type: "input",
+      name: "idnum",
+      message: "What is your ID number?"
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "What is your role with the company?"
+    },
+  ]);
+}
+
+function generateHTML(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Team-Profile</title>
+</head>
+<body>
+ 
+</body>
+</html>`;
+}
+
+promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
